97d0c3cd233228d657788849d26ab05d274f115b
Janne H?rk?nen <jth@reaktor.fi>
2011-11-17 14:11:39 +0200

    test cleanup

--- atest/libraries/VacalcLibrary.py
+++ atest/libraries/VacalcLibrary.py
@@ -3,7 +3,16 @@ import tempfile
 
 from vacalc.employeestore import EmployeeStore
 
-def get_all_employee_names():
+
+def get_employee_names():
     default_db = os.path.join(tempfile.gettempdir(), 'vacalcdb.csv')
     db_file= os.environ.get('VACALC_DB', default_db)
     return [emp.name for emp in EmployeeStore(db_file).get_all_employees()]
+
+
+def application_should_have_employees(expected):
+    expected = [name.strip() for name in expected.split(',')]
+    actual = get_employee_names()
+    if actual != expected:
+        raise AssertionError('Expected employees: %s\nActual employees: %s'
+                             % (', '.join(expected), ', '.join(actual)))
